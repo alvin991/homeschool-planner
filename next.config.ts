ca7fda6146dcec3facbd4d58777e4b28d6c9c456
alvin991 <alvin991@gmail.com>
2026-03-01 07:34:24 +0000

    Use webpack watchOptions to check file changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+        ignored: /node_modules|.next/,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
